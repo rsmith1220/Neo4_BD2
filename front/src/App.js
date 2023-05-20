fa3a9984965ba73af6054fd1017d26a3379658bf
Rebecca Smith <rebecca.smith1220.rs@gmail.com>
2023-05-19 18:10:56 -0600

    creacion de react app

---
+++ front/src/App.js
@@ -0,0 +1,17 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header>
+        
+        <p className='titulo-principal'>
+          Contenedor de libros punto com
+        </p>
+
+      </header>
+    </div>
+  );
+}
+
+export default App;
