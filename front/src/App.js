8fe6361d08d04f13d7820f7c5247fc25ff08b702
Rebecca Smith <rebecca.smith1220.rs@gmail.com>
2023-05-29 12:14:27 -0600

    updated front

--- front/src/App.js
+++ front/src/App.js
@@ -2,14 +2,30 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
+    <div className="app">
       <header>
         
         <p className='titulo-principal'>
           Contenedor de libros punto com
         </p>
-
       </header>
+    <div className = 'buscador'>
+      <p>Nombre del libro</p>
+      <input>
+      </input>
+    </div>
+    
+<div className='contiene'>
+  <p className='infor'>El libro esta en WAREHOUSE</p>
+
+    <p className='infor'>El supplier es SUPPLIER</p>
+
+    <p className='infor'>Se puede encontrar en RETAILERS</p>
+
+    <p className='infor'>Ha sido comprado por CUSTOMERS</p>
+
+</div>
+    
     </div>
   );
 }
