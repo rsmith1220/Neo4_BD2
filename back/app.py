c44f0731e60c3de813bac4863f0029dc3025690d
JoseGonzalez098 <gon20335@uvg.edu.gt>
2023-05-31 11:40:09 -0600

    back

--- back/app.py
+++ back/app.py
@@ -57,11 +57,17 @@ def create_node_query(labels, properties):
 
 @app.route('/create_node', methods=['POST'])
 def create_node():
-    session = get_neo4j_session()
+    driver = get_neo4j_session()
     data = request.get_json()
-    query = create_node_query(data['labels'], data['properties'])
-    result = session.run(query)
-    node = [dict(record['n']) for record in result]
+    # Execute the Cypher query
+    with driver.session() as session:
+        query = create_node_query(data['labels'], data['properties'])
+        result = session.run(query)
+        node = [dict(record['n']) for record in result]
+
+    # Close the Neo4j driver
+    driver.close()
+
     return jsonify(node)
 
 
@@ -79,13 +85,196 @@ def create_relationship_query(de, a, properties, relationship):
 
 @app.route('/create_relationship', methods=['POST'])
 def create_relationship():
-    session = get_neo4j_session()
+    driver = get_neo4j_session()
     data = request.get_json()
-    query = create_relationship_query(
-        data['de'], data['a'], data['properties'], data['relationship'])
-    result = session.run(query)
-    node = [dict(record['n']) for record in result]
+
+    # Execute the Cypher query
+    with driver.session() as session:
+        query = create_relationship_query(
+            data['de'], data['a'], data['properties'], data['relationship'])
+        result = session.run(query)
+        node = [dict(record['n']) for record in result]
+
+    # Close the Neo4j driver
+    driver.close()
+
     return jsonify(node)
+
+
+@app.route('/get_relation', methods=['POST'])
+def get_relationships_as_dictionary():
+    # Connect to Neo4j
+    driver = get_neo4j_session()
+
+    # Execute the Cypher query
+    with driver.session() as session:
+        result = session.run(
+            "MATCH (a)-[r]->(b) RETURN a.Name AS from, type(r) AS relation, b.Name AS to")
+
+        # Process the query result and store it in a list of dictionaries
+        relationships = []
+        for record in result:
+            relationship = {
+                'from': record['from'],
+                'relation': record['relation'],
+                'to': record['to']
+            }
+            relationships.append(relationship)
+
+    # Close the Neo4j driver
+    driver.close()
+
+    return relationships
+
+
+def update_node_query(labels, properties, new_properties):
+    labels_str = ":".join(labels)
+    formatted_properties = {
+        key: f"'{value}'" if isinstance(value, str) else str(value)
+        for key, value in properties.items()
+    }
+    property_str = " AND ".join(
+        f"n.{key} = {value}" for key, value in formatted_properties.items())
+
+    formatted_new_properties = {
+        key: f"'{value}'" if isinstance(value, str) else str(value)
+        for key, value in new_properties.items()
+    }
+    new_property_str = ", ".join(
+        f"n.{key} = {value}" for key, value in formatted_new_properties.items())
+    query = f"MATCH (n:{labels_str}) WHERE {property_str} SET {new_property_str}"
+
+    return query
+
+
+@app.route('/update_node', methods=['POST'])
+def update_node(labels, properties, new_properties):
+    driver = get_neo4j_session()
+    # Execute the Cypher query
+    with driver.session() as session:
+        query = update_node_query(labels, properties, new_properties)
+        result = session.run(query)
+    driver.close()
+
+    return {'status': "success"}
+
+
+def update_relationship_query(de, a, properties, relationship):
+    formatted_properties = {
+        key: f"'{value}'" if isinstance(value, str) else str(value)
+        for key, value in properties.items()
+    }
+    property_str = ", ".join(
+        f"r.{key} = {value}" for key, value in formatted_properties.items())
+    query = f"MATCH (a)-[r:{relationship}]->(b) WHERE a.name = '{de}' AND b.name = '{a}' SET {property_str}"
+
+    return query
+
+
+@app.route('/update_relationship', methods=['POST'])
+def update_relationship(de, a, properties, relationship):
+    driver = get_neo4j_session()
+    # Execute the Cypher query
+    with driver.session() as session:
+        query = update_relationship_query(de, a, properties, relationship)
+        result = session.run(query)
+    driver.close()
+
+    return {'status': "success"}
+
+
+def delete_node_properties_query(labels, properties, properties_to_delete):
+    labels_str = ":".join(labels)
+    formatted_properties = {
+        key: f"'{value}'" if isinstance(value, str) else str(value)
+        for key, value in properties.items()
+    }
+    property_str = " AND ".join(
+        f"n.{key} = {value}" for key, value in formatted_properties.items())
+    properties_to_delete_str = ", ".join(
+        f"n.{key}" for key in properties_to_delete)
+    query = f"MATCH (n:{labels_str}) WHERE {property_str} REMOVE {properties_to_delete_str}"
+
+    return query
+
+
+@app.route('/delete_node_properties', methods=['POST'])
+def delete_node_properties(labels, properties, properties_to_delete):
+    driver = get_neo4j_session()
+    # Execute the Cypher query
+    with driver.session() as session:
+        query = delete_node_properties_query(
+            labels, properties, properties_to_delete)
+        result = session.run(query)
+    driver.close()
+
+    return {'status': "success"}
+
+
+def delete_relationship_properties_query(de, a, relationship, properties_to_delete):
+
+    formatted_properties_to_delete = ", ".join(
+        f"r.{key}" for key in properties_to_delete)
+
+    query = f"MATCH (a)-[r:{relationship}]->(b) WHERE a.name = '{de}' AND b.name = '{a}' REMOVE {formatted_properties_to_delete}"
+
+    return query
+
+
+@app.route('/delete_relationship_properties', methods=['POST'])
+def delete_relationship_properties(de, a, relationship, properties_to_delete):
+    driver = get_neo4j_session()
+    # Execute the Cypher query
+    with driver.session() as session:
+        query = delete_relationship_properties_query(
+            de, a, relationship, properties_to_delete)
+        result = session.run(query)
+    driver.close()
+
+    return {'status': "success"}
+
+
+def delete_relationship_query(de, a, relationship):
+    query = f"MATCH (a)-[r:{relationship}]->(b) WHERE a.name = '{de}' AND b.name = '{a}' DELETE r"
+
+    return query
+
+
+@app.route('/delete_relationship', methods=['POST'])
+def delete_relationship(de, a, relationship):
+    driver = get_neo4j_session()
+    # Execute the Cypher query
+    with driver.session() as session:
+        query = delete_relationship_query(de, a, relationship)
+        result = session.run(query)
+    driver.close()
+
+    return {'status': "success"}
+
+
+def delete_node_query(labels, properties):
+    labels_str = ":".join(labels)
+    formatted_properties = {
+        key: f"'{value}'" if isinstance(value, str) else str(value)
+        for key, value in properties.items()
+    }
+    property_str = ", ".join(
+        f"{key}: {value}" for key, value in formatted_properties.items())
+    query = f"MATCH (n:{labels_str} {{{property_str}}}) DETACH DELETE n"
+
+    return query
+
+
+@app.route('/delete_node', methods=['POST'])
+def delete_node(labels, properties):
+    driver = get_neo4j_session()
+    # Execute the Cypher query
+    with driver.session() as session:
+        query = delete_node_query(labels, properties)
+        result = session.run(query)
+    driver.close()
+
+    return {'status': "success"}
 
 
 if __name__ == '__main__':
